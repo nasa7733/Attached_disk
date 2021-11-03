94e686709827ed5e71a7d75e8b6c1cb22a1706ae
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-11-03 13:53:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 provider "google" {
 
-project     = "midevops"
+project     = "my-project-id"
 region      = "us-central1"
 
 }
 
-resource "google_compute_instance" "default" {
+resource "google_compute_attached_disk" "first-disk" {
+  disk     =  "disk-1"
+  instance =  "test01"
+  project     = "my-project-id"
+  zone         = "us-central1-a"
+}
+
+resource "google_compute_instance" "first-vm" {
   name         = "test01"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
@@ -29,21 +36,9 @@ boot_disk {
 
      metadata_startup_script = "echo hi > /test.txt"
 
-   //  service_account {
-   //      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-   //      email  = 584230291121-compute@developer.gserviceaccount.com
-   //      scopes = ["cloud-platform"]
 
 
-     
          lifecycle {
-    ignore_changes = [attached_disk]
+    ignore_changes = ["attached_disk"]
 }
-      
-  }
-
-
-  resource "google_compute_attached_disk" "default" {
-  disk     =  disk-1
-  instance =  test01
-}
+       }
