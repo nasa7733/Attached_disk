6919084b15b29892141edd462415fd7c444a709a
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-11-10 14:00:35 +0530

    Update main_disk.tf

--- main_disk.tf
+++ main_disk.tf
@@ -1,5 +1,6 @@
 resource "google_compute_attached_disk" "first-disk" {
   disk     =  "apollo-data"
+  device_name =  "apollo-data"
   instance =  "test01"
   project     = "midevops"
   zone         = "us-west4-b"
