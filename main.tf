7eb6366947f321da0c251f85c553bc4f8dafc07b
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-11-23 20:02:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ region      = "us-central1"
 
 }
 
+resource "google_compute_attached_disk" "first-disk" {
+  disk     =  "disk-1"
+  instance =  "test01"
+  project     = "my-project-id"
+  zone         = "us-central1-a"
+}
+
 resource "google_compute_instance" "first-vm" {
   name         = "test01"
   machine_type = "f1-micro"
