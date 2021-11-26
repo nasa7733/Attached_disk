9b9d38eefa583b60660cac7b68ad1477528ab6b5
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-11-26 12:36:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ boot_disk {
 
 metadata = {
     startup_script = "sudo apt-get install lvm2 -y"
-    startup_script =  "./Scripts/startup.sh"
+    startup_script =  "./Scripts/startup.sh" "./Scripts/output.sh"
 // startup_script =  file("${path.module}/Scripts/output.sh")
   
   //  startup-script-url = "gs://tfstate-midevops/startupscripts/scripts.sh"
