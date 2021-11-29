00a69a12d08152138d0038276d8d69cc9cb570d7
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-11-29 21:40:42 +0530

    Update startup.sh

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -4,3 +4,7 @@ sudo apt-get install lvm2 -y
 sudo vgchange -ay sos
 sudo mkdir -p /userdata
 sudo mount /dev/sos/vol01 /userdata
+sudo groupadd -g 234 Nasa
+sudo more /etc/group | grep Nasa
+sudo useradd -g 234 test_user
+sudo more /etc/passwd | grep test_user
