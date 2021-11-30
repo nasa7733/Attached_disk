ad8c2e4a21c580e4ded476fb647ad2a082c50740
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-11-30 12:41:15 +0530

    Update output.sh

--- Scripts/output.sh
+++ Scripts/output.sh
@@ -1,17 +1,19 @@
 cat >> /userdata/outfile.txt <<EOF
 
 Deploying a Webserver with Terraform on GCP
-Sameed Uddin Mohammed
-Sameed Uddin Mohammed
+Narayanaswamy Chitra  
+Ishaq Mohammed
 Follow
 
 Jun 28 · 5 min read
-Picture by David Aquino
+Picture by Nasa
 In this project, we will learn how to use Terraform with GCP but also create a Apache Webserver on the instance (f1-micro) using bash script.
 Overview of the following products & services being used in this project:
 
 Terraform
-Terraform is an open-source cloud agnostic infrastructure as code tool that provides a consistent CLI workflow to manage hundreds of cloud vendor-specific services. Terraform codifies cloud APIs into declarative configuration files.Most Popular cloud providers like GCP|AWS|Azure all provide their own version of Infra-as-code tools:
+Terraform is an open-source cloud agnostic infrastructure as code tool that provides a consistent CLI workflow to manage hundreds of cloud vendor-specific services. 
+Terraform codifies cloud APIs into declarative configuration files.
+Most Popular cloud providers like GCP|AWS|Azure all provide their own version of Infra-as-code tools:
 
 
 EOF
